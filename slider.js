1766ffbfc4cd1da1d420dcb85ac6de0eb7fcfac7
Tonton404 <victor.anton@hotmail.fr>
2017-06-28 12:07:35 +0000

    Added slider creation. Except the 1st one, all images are hidden

--- slider.js
+++ slider.js
@@ -1,18 +1,46 @@
 $(document).ready(function() {
+  var sliderHTML = "";
+  var sliderData;
+  var height= "500";
   initSlider();
+  createSlider();
+  render();
+
 
 
   function initSlider()
   {
     // Here, we load the HTML hook.
-    var sliderData = $('#slider-container img');
+    sliderData = $('#slider-container img');
 
     // If it is broken, it throws an error to stop the script.
-    is(sliderData.length == 0)  {
+    if(sliderData.length == 0)  {
       throw "Erreur de selection";
     }
     $('#slider-container').remove();
   }
 
+  function render()
+  {
+    $('body').append(sliderHTML);
+  }
+  function createSlider()
+  {
+    sliderHTML += "<div id='slider-container' style='display:flex;flex-direction:row;overflow:hidden;width:100%;min-height:100vh;max-height:100%;position:absolute;top:0;left:0;margin:0auto;'>";
+      for (var i = 0; i < sliderData.length; i++) {
+        var display = "";
+        if(i ==0);  {
+          display ="display:none;justify-content:center;";
+        } else {
+          display="display=none;";
+        }
+      sliderHTML += "<div style='width:100%;height:500px;" + display + "'>";
+      sliderHTML += "<img src='" + $(sliderData[i])[0].src + "' style='height:" + height + "px;width:auto;'>";
+      sliderHTML += "</div>";
+      }
+    sliderHTML += "</div>";
+    sliderHTML += "button data-action=>"Left"><</button>";
+    sliderHTML += "button data-action=>"Right"><</button>";
+  }
 
 });
